5afb9c9c4d60ff1912cb85b08d7dfb50aceaef89
NevoTapiero <nevotap1234@gmail.com>
2023-10-02 03:42:42 +0300

    forgot this one

--- lib/Posts Process/post_service.dart
+++ lib/Posts Process/post_service.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: depend_on_referenced_packages, no_leading_underscores_for_local_identifiers
-import 'dart:math' as math;
 import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
